c41c52fe516b91ec66811925c3cb3241e92b133c
AndreFCruz <andrecruz97@gmail.com>
2017-03-20 15:22:36 +0000

    Updated high_level

--- high_level.py
+++ high_level.py
@@ -1,1 +1,10 @@
-import lib.py
+from lib import *
+
+def draw_letter(letter='A'):
+	print("draw letter A")
+	roll_forward()
+
+def draw_letter(letter='B'):
+	print("draw letter B")
+
+draw_letter('B')
